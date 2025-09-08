777f21d50b6b5af7bfc2dc4625014894c2ab02cc
Tens1des <romankotov3213@gmail.com>
2025-09-08 22:47:03 +0300

    Initial Commit

---
+++ Shard Escape/Shard_EscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shard_EscapeApp.swift
+//  Shard Escape
+//
+//  Created by Рома Котов on 08.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Shard_EscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
